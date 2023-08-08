88f5211424d53b23e3592fcf5ebffd623b3816e3
localuser@setup <localuser@setup>
2023-08-08 16:23:45 +0200

    Recalibrated, lowered setup; current CMN protocol;

--- protocols/th_cmn.py
+++ protocols/th_cmn.py
@@ -1,5 +1,10 @@
+import numpy as np
+
 import vxpy.core.protocol as vxprotocol
-from visuals.spherical_global_motion import CMN_100_000f_20fps_10tp_0p1sp
+from visuals.spherical_global_motion import (CMN_100_000f_20fps_10tp_0p1sp,
+                                             CMN_15_000f_15fps_10tp_0p1sp_0p03ns,
+                                             CMN_15_000f_15fps_10tp_0p1sp_0p03ns_inv)
+from visuals.spherical_global_motion import TranslationGrating
 from vxpy.visuals.spherical_uniform_background import SphereUniformBackground
 
 
@@ -18,4 +23,45 @@ class CMN20230804(vxprotocol.StaticProtocol):
 
         p = vxprotocol.Phase(15)
         p.set_visual(SphereUniformBackground)
+        self.add_phase(p)
+
+
+class CMN20230808(vxprotocol.StaticProtocol):
+
+    def create(self):
+
+        # p = vxprotocol.Phase(15)
+        # p.set_visual(SphereUniformBackground, {SphereUniformBackground.u_color: [0.5, 0.5, 0.5]})
+        # self.add_phase(p)
+
+        for i in range(3):
+            for azim in np.arange(-180, 180, 45):
+
+                phase = vxprotocol.Phase(duration=4)
+                phase.set_visual(TranslationGrating,
+                                 {TranslationGrating.azimuth: azim,
+                                  TranslationGrating.elevation: 0.0,
+                                  TranslationGrating.angular_period: 20,
+                                  TranslationGrating.angular_velocity: 0})
+                self.add_phase(phase)
+
+                phase = vxprotocol.Phase(duration=4)
+                phase.set_visual(TranslationGrating,
+                                 {TranslationGrating.azimuth: azim,
+                                  TranslationGrating.elevation: 0.0,
+                                  TranslationGrating.angular_period: 20,
+                                  TranslationGrating.angular_velocity: 30})
+                self.add_phase(phase)
+
+
+        phase = vxprotocol.Phase(duration=15*60)
+        phase.set_visual(CMN_15_000f_15fps_10tp_0p1sp_0p03ns)
+        self.add_phase(phase)
+
+        phase = vxprotocol.Phase(duration=15*60)
+        phase.set_visual(CMN_15_000f_15fps_10tp_0p1sp_0p03ns_inv)
+        self.add_phase(phase)
+
+        p = vxprotocol.Phase(15)
+        p.set_visual(SphereUniformBackground, {SphereUniformBackground.u_color: [0.5, 0.5, 0.5]})
         self.add_phase(p)
