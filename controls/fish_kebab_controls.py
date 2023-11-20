ab63d8108a456da68ca183d78a9e5bb9f87d9b22
thladnik <tim.hladnik@gmail.com>
2023-11-20 13:23:45 +0100

    Semi-final fixes for fish kebab experiments;

--- controls/fish_kebab_controls.py
+++ controls/fish_kebab_controls.py
@@ -4,20 +4,44 @@ import vxpy.core.attribute as vxattribute
 import vxpy.core.control as vxcontrol
 import vxpy.core.devices.serial as vxserial
 import vxpy.core.container as vxcontainer
+from devices.telemetrix_stepper import TelemetrixStepperKebab
+import vxpy.core.container as vxcontainer
 
 
 class Control01(vxcontrol.BaseControl):
+
+    device: TelemetrixStepperKebab
+    velocity: int = None
+    direction: int = None
+    led_left_state = 0.
+    led_right_state = 0.
 
     def __init__(self, *args, **kwargs):
         vxcontrol.BaseControl.__init__(self, *args, **kwargs)
 
         self.device = vxserial.get_serial_device_by_id('Dev_kebab')
 
+    @staticmethod
+    def _led_state_output(value: float) -> int:
+        return int(value * 2**16)
+
     def initialize(self, **kwargs):
-        pass
+        self.device.board.stepper_set_max_speed(self.device.motor, 1000)
+        self.device.board.stepper_set_speed(self.device.motor, 50 * self.velocity)
+        self.device.board.stepper_run_speed(self.device.motor)
+
+        self.device.board.analog_write(self.device.led_left, self._led_state_output(self.led_left_state))
+        self.device.board.analog_write(self.device.led_right, self._led_state_output(self.led_right_state))
+
+        # Add datasets to be saved to file
+        vxcontainer.add_phase_attributes({'velocity': self.velocity, 'direction': self.direction})
+        vxcontainer.create_phase_dataset('led_left', (1,), np.float32)
+        vxcontainer.create_phase_dataset('led_right', (1,), np.float32)
 
     def main(self, dt, **pins):
-        vxserial.write_pin('led_ctrl1', np.random.rand() < 0.05)
-        vxserial.write_pin('led_ctrl2', np.random.rand() < 0.05)
 
-        vxcontainer.add_to_dataset('some_position', np.random.randint(100))
+        self.device.board.analog_write(self.device.led_left, self._led_state_output(self.led_left_state))
+        self.device.board.analog_write(self.device.led_right, self._led_state_output(self.led_right_state))
+
+        vxcontainer.add_to_phase_dataset('led_left', self.led_left_state)
+        vxcontainer.add_to_phase_dataset('led_right', self.led_right_state)
--- plugins/telemetrix_stepper_feedback.py
+++ plugins/telemetrix_stepper_feedback.py
@@ -12,32 +12,40 @@ class KebabPositionTracker(vxroutine.IoRoutine):
 
     steps_to_full_rotation: int = 3200
     callback_is_set = False
+    rotation_num = 0
+    wait_for_position = False
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
         vxattribute.ArrayAttribute('stepper_position', (1,), vxattribute.ArrayType.int64)
+        vxattribute.ArrayAttribute('stepper_rotation_number', (1,), vxattribute.ArrayType.uint64)
         vxattribute.ArrayAttribute('stepper_full_rotation_trigger', (1,), vxattribute.ArrayType.bool)
 
     def initialize(self):
         vxui.register_with_plotter('stepper_position', axis='stepper position')
+        vxui.register_with_plotter('stepper_rotation_number')
         vxui.register_with_plotter('stepper_full_rotation_trigger')
+        vxattribute.write_to_file(self, 'stepper_position')
+        vxattribute.write_to_file(self, 'stepper_rotation_number')
+        vxattribute.write_to_file(self, 'stepper_full_rotation_trigger')
         self.i = 0
 
     def main(self):
-        # if not self.callback_is_set:
         self.device = vxserial.get_serial_device_by_id('Dev_kebab')
-        if self.i % 10 == 0:
+        # if self.i % 1 == 0:
+        if not self.wait_for_position:
             self.device.board.stepper_get_current_position(self.device.motor, self._current_position_callback)
-
-        if self.i % 1000 == 0:
-            self.device.board.stepper_set_max_speed(self.device.motor, 1000)
-            self.device.board.stepper_set_speed(self.device.motor, 50 * np.random.randint(10))
-            self.device.board.stepper_run_speed(self.device.motor)
-        self.i += 1
-        # print('Muh')
+            self.wait_for_position = True
 
     def _current_position_callback(self, position: list):
-        print(position)
         vxattribute.write_attribute('stepper_position', position[2])
-        vxattribute.write_attribute('stepper_full_rotation_trigger', position[2] % self.steps_to_full_rotation)
+
+        rotation_num = position[2] // self.steps_to_full_rotation
+
+        vxattribute.write_attribute('stepper_full_rotation_trigger', rotation_num > self.rotation_num)
+        self.rotation_num = rotation_num
+
+        vxattribute.write_attribute('stepper_rotation_number', self.rotation_num)
+
+        self.wait_for_position = False
