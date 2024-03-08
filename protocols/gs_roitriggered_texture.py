341555aea957371044df8d71a06960cdc5343b59
Giulia2906 <giulia.hindermann@gmail.com>
2024-03-08 15:48:13 +0100

    Piezo experiment protocol - GS

--- protocols/gs_roitriggered_texture.py
+++ protocols/gs_roitriggered_texture.py
@@ -3,39 +3,72 @@ import numpy as np
 import vxpy.core.protocol as vxprotocol
 import vxpy.core.event as vxevent
 
-from visuals.texture_flash_saccadeTrigger.saccade_flash_saccadeTrigger import SaccadeFlash_SaccadeTriggerStimulation4000, SaccadeFlash_SaccadeTriggerStimulation2000 # phase saccade triggered
-from visuals.gs_saccadic_suppression.gs_simu_saccade_sine_flash import SimuSaccadeWithSineFlash2000
-from visuals.gs_saccadic_suppression.gs_simu_saccade_sine_flash import SimuSaccadeWithSineFlash4000
-from vxpy.visuals.spherical_uniform_background import SphereUniformBackground  # for setting screen to black
+from vxpy.visuals.spherical_uniform_background import SphereUniformBackground
+from visuals.gs_SaccadeTriggeredTextDisplCombined.SaccadeTriggeredTextDisplCombined import TextureRotationCosineFlash
+
+def paramstext(rot_duration, rot_start, rot_amp, rot_dir, flash_start, flash_dur, flash_amp, flash_freq, baseline_lum, contrast):
+    return {
+        TextureRotationCosineFlash.rotation_duration: rot_duration,
+        TextureRotationCosineFlash.rotation_start_time: rot_start,
+        TextureRotationCosineFlash.rotation_amplitude: rot_amp,
+        TextureRotationCosineFlash.rotation_direction: rot_dir,
+        TextureRotationCosineFlash.flash_start_time: flash_start,
+        TextureRotationCosineFlash.flash_amp: flash_amp,
+        TextureRotationCosineFlash.flash_duration: flash_dur,
+        TextureRotationCosineFlash.flash_freq: flash_freq,
+        TextureRotationCosineFlash.baseline_lum: baseline_lum,
+        TextureRotationCosineFlash.contrast: contrast
+    }
 
 
 class ROITriggered_Coarse(vxprotocol.TriggeredProtocol):
     def __init__(self, *args, **kwargs):
         vxprotocol.TriggeredProtocol.__init__(self, *args, **kwargs)
 
-        # set parameters (phases saccade triggered)
-        flash_conditions_list = [0.02, 0.1, 0.25, 0.5, 1, 2, 4, -1, 6.5, 6.5] # -1 for flash whithout saccade (wie oft soll das gezeigt werden?)
-        flash_conditions_numIter = 1  # eigentlich 4 -> da links und rechts oder noch mehr
+        np.random.seed(1)
 
-        flash_conditions_fullList = np.repeat(flash_conditions_list, flash_conditions_numIter)  # np.array with all iterations of stimulus conditions
-        np.random.seed(0)  # fix seed
-        flash_conditions_shuffeled = flash_conditions_fullList.copy()  # make deep (not affecting) copy of condition list
-        np.random.shuffle(flash_conditions_shuffeled)  # shuffle condition List
+        # set fixed parameters texture
+        rot_duration = 0
+        rot_start = 0
+        rot_amp = 0
+        rot_dir = 0
+        flash_dur_text = 500
+        flash_freq_text = 2
+        baseline_lum_text = 0.75
+        contrast = 0.5
+
+
+        # experimental conditions, (delay, flash_amp) In total 10 Conditions:
+        # 7 delays, 1 ctrl saccade (no flash, delay = 333), 2 ctrl flash (8s delay) = 10 conditions
+        conditions = [(20, 0.5), (100, 0.5), (250, 0.5), (500, 0.5), (1000, 0.5), (2000, 0.5), (4000, 0.5), (333, 0),
+                      (8000, 0.5), (8000, 0.5)]
 
         # Set tigger that controls progression of this protocol
         trigger = vxevent.RisingEdgeTrigger('roi_activity_trigger')
         self.set_phase_trigger(trigger)
 
-        # delay für 1. phase
-        # 30 seconds just texture (no flash)
-        p = vxprotocol.Phase(duration=10)
-        p.set_visual(SimuSaccadeWithSineFlash2000,
-                     {SimuSaccadeWithSineFlash2000.saccade_duration: 100,
-                      SimuSaccadeWithSineFlash2000.saccade_start_time: 1500,
-                      SimuSaccadeWithSineFlash2000.saccade_target_angle: 0,
-                      SimuSaccadeWithSineFlash2000.sine_start_time: 1500,
-                      SimuSaccadeWithSineFlash2000.sine_duration: 500,
-                      SimuSaccadeWithSineFlash2000.sine_amp: 0,
-                      SimuSaccadeWithSineFlash2000.sine_freq: 2,
-                      SimuSaccadeWithSineFlash2000.baseline_lum: 0.75,
-                      SimuSaccadeWithSineFlash2000.contrast: 0.5})
+        repeats = 4
+
+        for i in range(repeats):
+            # 15 sec just texture (flash delay = 0)
+            p = vxprotocol.Phase(duration=15)
+            p.set_visual(TextureRotationCosineFlash,
+                         paramstext(rot_duration, rot_start, rot_amp, rot_dir, 0, flash_dur_text,
+                                    0, flash_freq_text, baseline_lum_text, contrast))
+            self.add_phase(p)
+
+            # shuffled conditions, texture
+            for delay, flash_amp in np.random.permutation(conditions):
+                if delay > 4000:
+                    phase_dur = 12
+                else:
+                    phase_dur = 8
+                p = vxprotocol.Phase(phase_dur)
+                p.set_visual(TextureRotationCosineFlash, paramstext(rot_duration, rot_start, rot_amp, rot_dir, delay,
+                                                                    flash_dur_text, flash_amp, flash_freq_text,
+                                                                    baseline_lum_text, contrast))
+                self.add_phase(p)
+
+        p = vxprotocol.Phase(duration=5)
+        p.set_visual(SphereUniformBackground, {SphereUniformBackground.u_color: np.array([0, 0, 0])})
+        self.add_phase(p)
