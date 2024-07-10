7c04bc15e6c9cd5d2f9cc4db75d62f070eaa0abe
thladnik <tim.github@hladnik.de>
2024-07-10 12:36:00 +0200

    Add time differential rotation sphere;

--- visuals/spherical_global_motion/__init__.py
+++ visuals/spherical_global_motion/__init__.py
@@ -6,4 +6,4 @@ from .cmn import ContiguousMotionNoise, \
     CMN_3_000f_15fps_7tp_0p25sp_0p005ns, \
     CMN_20_000f_15fps_7tp_0p25sp_0p005ns
 from .optic_flow import GlobalOpticFlow
-from .motion_in_sphere import TranslationGrating, RotationGrating
+from .motion_in_sphere import TranslationGrating, RotationGrating, Football
