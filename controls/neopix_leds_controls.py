995b3d28ee6758d3d710bd8507c04c92063e00e9
student@behavioral_setup01 <tim.hladnik@gmail.com>
2024-11-08 15:16:21 +0100

    add ramping of LEDs in neopix controls

--- controls/neopix_leds_controls.py
+++ controls/neopix_leds_controls.py
@@ -27,7 +27,7 @@ class ControlTest(vxcontrol.BaseControl):
         self.device.set_led(self.led_num, [self.red_ch, self.green_ch, self.blue_ch])
 
         # Add datasets to be saved to file
-        vxcontainer.add_phase_attributes({'velocity': self.led_num, 'red_ch': self.red_ch, 'green_ch': self.green_ch, 'blue_ch': self.blue_ch})
+        vxcontainer.add_phase_attributes({'led_num': self.led_num, 'red_ch': self.red_ch, 'green_ch': self.green_ch, 'blue_ch': self.blue_ch})
 
         #vxcontainer.create_phase_dataset('led_left', (1,), np.float32)
         #vxcontainer.create_phase_dataset('led_right', (1,), np.float32)
@@ -39,3 +39,74 @@ class ControlTest(vxcontrol.BaseControl):
     def _end(self):
         self.device.clear_pixels()
         print('-----END PHASE')
+
+
+class ControlNeopixRamp(vxcontrol.BaseControl):
+
+    device: NeopixSerial
+    led_num: int = None
+    red_ch: int = 0
+    green_ch: int = 0
+    blue_ch: int = 0
+
+    led_start_intensity: float = 0.
+    led_end_intensity: float = 0.
+    led_middle_intensity: float = 0.  # [%]
+
+    led_start_intensity_duration: int = 0
+    led_end_intensity_duration: int = 0
+    led_middle_intensity_duration: int = 0
+
+    def __init__(self, *args, **kwargs):
+        vxcontrol.BaseControl.__init__(self, *args, **kwargs)
+
+        self.device = vxserial.get_serial_device_by_id('Dev_neopix')
+
+    def initialize(self, **kwargs):
+
+        self.current_time = 0.
+        self.led_current_intensity = self.led_start_intensity
+
+        self.device.set_led(self.led_num,
+                            [int(float(self.red_ch)*self.led_current_intensity),
+                             int(float(self.green_ch) * self.led_current_intensity),
+                             int(float(self.blue_ch)*self.led_current_intensity)]
+                            )
+
+        # Add datasets to be saved to file
+        vxcontainer.add_phase_attributes(
+            {'led_num': self.led_num, 'red_ch': self.red_ch, 'green_ch': self.green_ch, 'blue_ch': self.blue_ch})
+
+        vxcontainer.create_phase_dataset('led_rgb_intensity', (1,), np.float32)
+
+    def main(self, dt, **pins):
+
+        self.current_time += dt
+
+        if self.current_time < self.led_start_intensity_duration:
+            self.led_current_intensity += (
+                        ((self.led_middle_intensity - self.led_start_intensity) / self.led_start_intensity_duration) * dt)
+
+        elif self.current_time > self.led_start_intensity_duration + self.led_middle_intensity_duration:
+            self.led_current_intensity += (
+                        ((self.led_end_intensity - self.led_middle_intensity) / self.led_end_intensity_duration) * dt)
+
+        else:
+            self.led_current_intensity = self.led_middle_intensity
+
+        self.device.set_led(self.led_num,
+                            [int(float(self.red_ch) * self.led_current_intensity),
+                             int(float(self.green_ch) * self.led_current_intensity),
+                             int(float(self.blue_ch) * self.led_current_intensity)]
+                            )
+
+        vxcontainer.add_to_phase_dataset('led_rgb_intensity', self.led_current_intensity)
+
+    def _end(self):
+        print('-----END PHASE')
+
+        self.device.set_led(self.led_num,
+                            [int(float(self.red_ch) * self.led_end_intensity),
+                             int(float(self.green_ch) * self.led_end_intensity),
+                             int(float(self.blue_ch) * self.led_end_intensity)]
+                            )
--- protocols/dsb_neopix_protocols.py
+++ protocols/dsb_neopix_protocols.py
@@ -47,3 +47,47 @@ class DLR_protocol_tvw(vxprotocol.StaticProtocol):
                                   {'led_num': led_num, 'red_ch': self.red_ch, 'green_ch': self.green_ch, 'blue_ch': self.blue_ch})
             self.add_phase(phase)
 
+
+class DLR_protocol_ramp_tvw(vxprotocol.StaticProtocol):
+
+    led_total_num = 21
+    red_ch = 105    # 0 - 255 daylight: sRGB = (255,241,234)
+    green_ch = 91
+    blue_ch = 84
+
+    def create(self):
+
+        for led_num in range(1, self.led_total_num+1):
+
+            #if led_num % 2 == 0:
+                #continue
+
+            # pause phase
+            phase = vxprotocol.Phase(duration=6)
+            phase.set_control(neopix_leds_controls.ControlNeopixRamp,
+                              {'led_num': led_num, 'red_ch': 0, 'green_ch': 0, 'blue_ch': 0,
+
+                               'led_start_intensity': 0,
+                               'led_middle_intensity': 0,
+                               'led_end_intensity': 0,
+
+                               'led_start_intensity_duration': 2,
+                               'led_middle_intensity_duration': 2,
+                               'led_end_intensity_duration': 2
+                               })
+            self.add_phase(phase)
+
+            # stimulation phase
+            phase = vxprotocol.Phase(duration=15)
+            phase.set_control(neopix_leds_controls.ControlNeopixRamp,
+                              {'led_num': led_num, 'red_ch': self.red_ch, 'green_ch': self.green_ch, 'blue_ch': self.blue_ch,
+
+                               'led_start_intensity': 0,
+                               'led_middle_intensity': 1,
+                               'led_end_intensity': 0,
+
+                               'led_start_intensity_duration': 5,
+                               'led_middle_intensity_duration': 5,
+                               'led_end_intensity_duration': 5
+            })
+            self.add_phase(phase)
