caa1c8436429095360e3fc19158d05a896606300
Giulia2906 <giulia.hindermann@gmail.com>
2025-11-18 16:39:08 +0100

    Protocol for tail tracking experiment during escape and prey stimulus. -GS

---
+++ protocols/gs_loomingescape_behavior.py
@@ -0,0 +1,129 @@
+from vxpy.core.protocol import StaticProtocol, Phase
+from visuals.gs_looming_disc.gs_looming_disc import LoomingDiscOnTexture2000
+from visuals.gs_looming_disc.gs_looming_disc import LoomingDiscOnTexture4000
+from visuals.gs_movingDotOnTexture.gs_dot_on_texture import MovingDotOnTexture2000
+from visuals.gs_movingDotOnTexture.gs_dot_on_texture import MovingDotOnTexture4000
+from visuals.gs_looming_disc.gs_simu_saccade_sine_flash import SimuSaccadeWithSineFlash2000
+from visuals.gs_looming_disc.gs_simu_saccade_sine_flash import SimuSaccadeWithSineFlash4000
+from vxpy.visuals.spherical_uniform_background import SphereUniformBackground
+import numpy as np
+
+
+def saccade2000(sacc_duration, sacc_start, sacc_ang, sine_start, sine_dur, sine_amp, sine_freq, baseline_lum, contrast):
+    return {
+        SimuSaccadeWithSineFlash2000.saccade_duration: sacc_duration,
+        SimuSaccadeWithSineFlash2000.saccade_start_time: sacc_start,
+        SimuSaccadeWithSineFlash2000.saccade_target_angle: sacc_ang,
+        SimuSaccadeWithSineFlash2000.sine_start_time: sine_start,
+        SimuSaccadeWithSineFlash2000.sine_duration: sine_dur,
+        SimuSaccadeWithSineFlash2000.sine_amp: sine_amp,
+        SimuSaccadeWithSineFlash2000.sine_freq: sine_freq,
+        SimuSaccadeWithSineFlash2000.baseline_lum: baseline_lum,
+        SimuSaccadeWithSineFlash2000.contrast: contrast
+    }
+
+
+
+def dot2000(luminance, contrast, motion_axis, dot_polarity, dot_start_ang, dot_ang_vel, dot_ang_diameter, dot_offset):
+    return {
+        MovingDotOnTexture2000.luminance: luminance,
+        MovingDotOnTexture2000.contrast: contrast,
+        MovingDotOnTexture2000.motion_axis: motion_axis,
+        MovingDotOnTexture2000.dot_polarity: dot_polarity,
+        MovingDotOnTexture2000.dot_start_angle: dot_start_ang,
+        MovingDotOnTexture2000.dot_angular_velocity: dot_ang_vel,
+        MovingDotOnTexture2000.dot_angular_diameter: dot_ang_diameter,
+        MovingDotOnTexture2000.dot_offset_angle: dot_offset
+    }
+
+
+def disc2000(luminance, contrast, motion_axis, disc_polarity, disc_azimuth, disc_elevation, disc_starting_diameter, disc_expansion_lv):
+    return {
+        LoomingDiscOnTexture2000.luminance: luminance,
+        LoomingDiscOnTexture2000.contrast: contrast,
+        LoomingDiscOnTexture2000.motion_axis: motion_axis,
+        LoomingDiscOnTexture2000.disc_polarity: disc_polarity,
+        LoomingDiscOnTexture2000.disc_azimuth: disc_azimuth,
+        LoomingDiscOnTexture2000.disc_elevation: disc_elevation,
+        LoomingDiscOnTexture2000.disc_starting_diameter: disc_starting_diameter,
+        LoomingDiscOnTexture2000.disc_expansion_lv: disc_expansion_lv
+    }
+
+
+
+class LoomingEscapeBehavior(StaticProtocol):
+
+    def __init__(self, *args, **kwargs):
+        StaticProtocol.__init__(self, *args, **kwargs)
+
+        self.global_visual_props['azim_angle'] = 0.
+
+        # Fix seed
+        np.random.seed(0)
+
+        # set fixed saccade parameters
+        sacc_duration = 8000    # in ms
+        sacc_ang = 240  # 30°/s * 8 sec = 240°
+        sine_start = 0
+        sacc_start = 0
+        sine_dur = 0
+        sine_freq = 0
+        sine_amp = 0
+        baseline_lum = 0.75
+        contrast = 0.5
+
+        # set fixed dot parameters
+        luminance = 0.75
+        motion_axis = 'vertical'
+        dot_polarity = 'dark-on-light'
+        dot_start_ang = 30
+        dot_ang_vel = -60
+        dot_ang_diameter = 20
+        dot_offset = 10
+
+        # set fixed dot parameters
+        disc_polarity = 'dark-on-light'
+        disc_azimuth = 0
+        disc_elevation = -90
+        disc_starting_diameter = 2  # in °
+        disc_expansion_lv = 240  # in ms
+
+        stimulus_choice = [1,2,3,4] # stimulus types: 1 = rotation grating CW, 2 = rotation grating CCW,3 = moving dot, 4 = looming disc
+        repeats = 10    # each stimulus tye gets repeated 10x
+        variants = np.array(stimulus_choice * repeats).reshape((-1,))
+
+
+
+        # all delay and saccade conditions in coarse
+        for stim_type in np.random.permutation(variants):
+
+            # 3 min just texture (pause phase)
+            p = Phase(duration=180)
+            p.set_visual(SimuSaccadeWithSineFlash2000, saccade2000(sacc_duration, sacc_start, 0, sine_start, sine_dur,
+                                                                   sine_amp, sine_freq, baseline_lum, contrast))
+            self.add_phase(p)
+
+            if stim_type == 1:
+                p = Phase(duration=8)
+                p.set_visual(SimuSaccadeWithSineFlash2000, saccade2000(sacc_duration, sacc_start, sacc_ang, sine_start, sine_dur,
+                                         sine_amp, sine_freq, baseline_lum, contrast))
+                self.add_phase(p)
+
+            elif stim_type == 2:
+                p = Phase(duration=8)
+                p.set_visual(SimuSaccadeWithSineFlash2000, saccade2000(sacc_duration, sacc_start, -sacc_ang, sine_start, sine_dur,
+                                         sine_amp, sine_freq, baseline_lum, contrast))
+                self.add_phase(p)
+
+            elif stim_type == 3:
+                p = Phase(duration=1.5)
+                p.set_visual(MovingDotOnTexture2000, dot2000(luminance, contrast, motion_axis, dot_polarity,
+                                                             dot_start_ang, dot_ang_vel, dot_ang_diameter, dot_offset))
+                self.add_phase(p)
+
+            elif stim_type == 4:
+                p = Phase(duration=2.5)
+                p.set_visual(LoomingDiscOnTexture2000, disc2000(luminance, contrast, motion_axis, disc_polarity,
+                                                                disc_azimuth, disc_elevation, disc_starting_diameter,
+                                                                disc_expansion_lv))
+                self.add_phase(p)
