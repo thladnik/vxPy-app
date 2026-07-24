b072a4dd38c25b8807abac7ab948ec4b2b762365
Giulia2906 <giulia.hindermann@gmail.com>
2026-07-24 16:40:12 +0200

    Update binary jitter visual to sample jitter angle uniformly and rotate by sqrt of angle*2

--- visuals/jitter_noise/jitter_noise.py
+++ visuals/jitter_noise/jitter_noise.py
@@ -105,7 +105,10 @@ class BinaryBlackWhiteJitterNoise(vxvisual.SphericalVisual):
 
     def generate_rotation(self):
 
-        angle = np.random.randint(5) * self.patch_diameter / 5 / 2
+        #angle = np.random.randint(5) * self.patch_diameter / 5 / 2
+        rng = np.random.default_rng()
+        x = rng.uniform(0, 5)
+        angle = np.sqrt(x * (self.patch_diameter/10) *2)    #replicates Gupta et al. by simulating a rotation by 10% of patch diameter on x and y, and then taking sqrt(x+y) to get diagonal as rotation angle. assumes rotation(x)=rotation(y)
 
         v3 = np.random.rand(3)
         v3 /= np.linalg.norm(v3)
